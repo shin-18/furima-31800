a3cdabc85bb9a84ea507d4ef2b873c5f234a5add
shin-18 <73209341+shin-18@users.noreply.github.com>
2020-11-27 01:53:05 +0900

    root修正２

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   get 'items/index'
   root 'items#index'
-=======
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> Stashed changes
+
 end
