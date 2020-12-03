696a83a3bf1adb512745f462c939eacd52edf5f6
shin-18 <73209341+shin-18@users.noreply.github.com>
2020-12-03 19:31:40 +0900

    修正４

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,103 +9,136 @@ describe User do
   before do
     @user = FactoryBot.build(:user)
   end
+
+   context 'ユーザーが正常に登録できる時' do
     it "nicknameとemail、passwordとpassword_confirmation、surname、name、surname_kana、name_kana、birthday_idが存在すれば登録できること" do
-      @user = build(:user)
       expect(@user).to be_valid
     end
 
+    it "パスワードが6文字以上であれば登録できること" do
+      @user.password = "123456"
+      expect(@user).to be_valid
+    end
+   end
+
+   context 'ユーザーが登録できない時' do
     it "ニックネームが空だと登録できない" do
-      @user = build(:user, nickname: "")
+      @user.nickname = nil
       @user.valid?
       expect(@user.errors[:nickname]).to include("を入力してください")
     end
 
     it "重複したemailが存在する場合登録できないこと" do
-      @user = create(:user)
       another_user = build(:user)
       another_user.valid?
       expect(another_user.errors[:email]).to include("すでに存在しています")
 
     it "メールアドレスが空だと登録できない" do
-      @user = build(:user, email: "")
+      @user.email = nil 
       @user.valid?
       expect(@user.errors[:email]).to include("を入力してください")
     end
 
+    it 'emailに＠が含まれていなければ登録ができないこと' do
+      @user.email = "bububububu"
+      @user.valid?
+      expect(@user.errors[:email]).to include("は不正な値です")
+    end
+
     it "パスワードが空だと登録できない" do
-      @user = build(:user, password: "")
+      @user.password = nil
       @user.valid?
       expect(@user.errors[:password]).to include("を入力してください")
     end
 
     it "性が空だと登録できない" do
-      @user = build(:user, surname: "")
+      @user.surname = nil
       @user.valid?
       expect(@user.errors[:surname]).to include("を入力してください")
     end
 
     it "名が空だと登録できない" do
-      @user = build(:user, name: "")
+      @user.name = nil
       @user.valid?
       expect(@user.errors[:name]).to include("を入力してください")
     end
 
     it "性カナが空だと登録できない" do
-      @user = build(:user, surname_kana: "")
+      @user.surname_kana = nil
       @user.valid?
       expect(@user.errors[:surname_kana]).to include("を入力してください")
     end
 
     it "名カナが空だと登録できない" do
-      @user = build(:user, name_kana: "")
+      @user.name_kana = nil
       @user.valid?
       expect(@user.errors[:name_kana]).to include("を入力してください")
     end
 
     it "パスワードと確認パスワードが一致していないと登録できない" do
-      @user = build(:user, password_confirmation: "")
+      @user.password_confirmation = ""
       @user.valid?
       expect(@user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
+    
+
     it "パスワードが6文字未満だと登録できない" do
-      @user = build(:user, password: "sample")
+      @user.password = "12345"
       @user.valid?
       expect(@user.errors[:password]).to include("は6文字以上で入力してください")
     end
   end
 
-  describe '#名前を半角で入力' do
+  it '半角英語のみでは登録できないこと' do
+    @user.password = "bububu"
+    @user.valid?
+    expect(@user.errors[:password]).to include("は不正な値です")
+  end
+
+  it '半角数字のみでは登録できないこと' do
+    @user.password = "123456"
+    @user.valid?
+    expect(@user.errors[:password]).to include("は不正な値です")
+  end
+
+  it '全角では登録できないこと' do
+    @user.password = "pg１２３４"
+    @user.valid?
+    expect(@user.errors[:password]).to include("は不正な値です")
+  end
+
+
     it '性を半角で入力した時登録できない' do
-      @user = build(:user, surname: "ﾐｮｳｼﾞ")
+      @user.surname = "ﾐｮｳｼ"
       @user.valid?
       expect(@user.errors[:surname]).to include("は全角で入力してください")
     end
  
     it '名を半角で入力した時登録できない' do
-      @user = build(:user, name: "ﾅﾏｴ")
+      @user.name = "ﾅﾏｴ"
       @user.valid?
       expect(@user.errors[:name]).to include("は全角で入力してください")
     end
   end
 
-  describe '#フリガナをカタカナ意外で入力' do
     it '性カナをカタカナ意外で入力した時登録できない' do
-      @user = build(:user, surname_kana: "kana")
+      @user.surname_kana = "kana"
       @user.valid?
       expect(@user.errors[:surname_kana]).to include("は全角カナで入力してください")
     end
  
     it '名カナをカタカナ意外で入力した時登録できない' do
-      @user = build(:user, name_kana: "kana")
+      @user.name_kana = "kana"
       @user.valid?
       expect(@user.errors[:name_kana]).to include("は全角カナで入力してください")
     end
   end
 
   it "birthdayがない場合は登録できないこと" do
-    @user = build(:user, birthday_id: nil)
+    @user.birthday_id = nil
     @user.valid?
     expect(@user.errors[:birthday_id]).to include("を入力してください")
   end
+ end
 end
