2fd7aa0fe68f9aceee9c03c58173f1e647c24a2c
shin-18 <73209341+shin-18@users.noreply.github.com>
2020-12-04 03:08:40 +0900

    ActiveHash導入、各選択実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,16 @@ class Item < ApplicationRecord
     belongs_to :user
     has_one :purchase
     has_one_attached :image
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :delivery_fee
+    belongs_to :area
+    belongs_to :days
 
     validates :image, presence: true
     validates :product_name, :explanation, presence: true
     validates :category_id, :status_id, :delivery_fee_id, :area_id, :days_id, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, presence: true
+    
 end
